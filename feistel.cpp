d6d86d1b8ab52fb0fdc56284177aed036223da2c
AtlasICL <ea616@exeter.ac.uk>
2025-10-07 01:25:00 +0100

    decryption

--- feistel.cpp
+++ feistel.cpp
@@ -3,7 +3,7 @@
 
 class FeistelNetwork {
 private:
-    const unsigned int ROUNDS;
+    const size_t ROUNDS;
     std::function<std::uint8_t(std::uint8_t, std::uint8_t)> SBox; // substitution function
 
     // Returns the 8 MSBs of the 16 bit value.
@@ -26,8 +26,7 @@ public:
     : ROUNDS(rounds), SBox(sbox) {}
 
     // Encrypts the given plaintext with the given key. Returns the ciphertext.
-    std::uint16_t const encrypt(std::uint16_t plaintext, std::uint8_t key) {
-
+    std::uint16_t encrypt(std::uint16_t plaintext, std::uint8_t key) const {
         std::uint8_t left = MSB8(plaintext);
         std::uint8_t right = LSB8(plaintext);
 
@@ -40,6 +39,24 @@ public:
             // Assign new L and R
             left = left_next;
             right = right_next;
+        }
+
+        return join(left, right);
+    }
+
+    std::uint16_t decrypt(std::uint16_t ciphertext, std::uint8_t key) const {
+        std::uint8_t left = MSB8(ciphertext);
+        std::uint8_t right = LSB8(ciphertext);
+
+        for (size_t round = 0; round < ROUNDS; round++) {
+
+            // Compute L_i-1 and R_i-1 (starting with L_n and R_n)
+            std::uint8_t right_prev = left;
+            std::uint8_t left_prev = SBox(left, key) ^ right;
+            
+            // Assign new L and R
+            left = left_prev;
+            right = right_prev;
         }
 
         return join(left, right);
