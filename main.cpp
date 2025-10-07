d975fee717a98ae0262ff064aae3bf9c3f9a19de
AtlasICL <ea616@exeter.ac.uk>
2025-10-07 01:35:49 +0100

    Working v1.0

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "feistel.cpp"
+
+#include <cstdint>
+#include <functional>
+#include <iostream>
+
+// Example substitution function. In this case, bitwise AND.
+std::uint8_t substitutionFunction(const std::uint8_t block, const std::uint8_t key) {
+    return block & key;
+}
+
+int main() {
+
+    FeistelNetwork FeistelCodec = FeistelNetwork(16, substitutionFunction);
+
+    const std::uint16_t plaintext = 12345;
+    const std::uint8_t key = 92;
+
+    const std::uint16_t ciphertext = FeistelCodec.encrypt(plaintext, key);
+    std::cout << "Ciphertext: " << ciphertext << std::endl;
+
+    const std::uint16_t retrieved_plaintext = FeistelCodec.decrypt(14377, key);
+    std::cout << "Retrieved plaintext: " << retrieved_plaintext << std::endl;
+}
